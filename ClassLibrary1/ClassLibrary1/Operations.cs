0f21e2a92dc2201dcf51ce728a4d221c1abaf6e4
Sana Eid <64756218+sanaomar7@users.noreply.github.com>
2022-02-18 01:53:46 +0200

    Add files via upload

---
+++ ClassLibrary1/ClassLibrary1/Operations.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ClassLibrary1
+{
+    public class Operations
+
+    {
+
+        // PRANCH 2 
+        public static int ReverseNumber(int number)
+        {
+            int reverseNum = 0, digit;
+            while (number != 0)
+            {
+                digit = number % 10;
+                reverseNum = (reverseNum * 10) + digit;
+                number = number / 10;
+            }
+
+
+            return reverseNum;
+
+        }
+
+
+        // BRANCH 3 
+        public static int BigestDigit(int number)
+        {
+            int digit1, digit2 = 0;
+            while (number != 0)
+            {
+                digit1 = number % 10;
+                if (digit2 < digit1)
+                {
+                    digit2 = digit1;
+                }
+                number = number / 10;
+            }
+
+
+            return digit2;
+
+        }
+
+
+        //BRANCH 4
+        public static int FirstNum(string stringValue)
+        {
+            string string2 = string.Empty;
+            int val = 0;
+            Console.WriteLine($"String with number: {stringValue}");
+            var matches = Regex.Matches(stringValue, @"\d+");
+            foreach (var match in matches)
+            {
+                string2 += match;
+            }
+            val = int.Parse(string2);
+            return val;
+
+        }
+
+
+        //BRANCH 1 
+        public static int MaximumInput(int numberE)
+        {
+            var max = -1000000000;
+
+
+            for (int i = 1; i <= numberE; i++)
+            {
+                Console.WriteLine("enter number " + i);
+                var num = int.Parse(Console.ReadLine());
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            Console.WriteLine("the maximum number from entered element = ");
+            return max;
+
+        }
+    }
+    }
+         
+    
---
+++ ClassLibrary1/ClassLibrary1/mainClass.cs
@@ -0,0 +1,86 @@
+﻿
+
+namespace ClassLibrary1
+{
+    class Controller
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.Write("-------  MENUE  ------ ");
+            Console.Write("\n1. REVERSE NUMBER  ----");
+            Console.Write("\n2. BIGEST DIGIT  ----");
+            Console.Write("\n3. FIRST NUMBER IN STRING   ----");
+            Console.Write("\n4. MAXIMUM INPUT NUMBER  ----");
+            Console.Write("\n5. EXIT  ----");
+
+            int choice;
+            do
+            {
+                bool result = int.TryParse(Console.ReadLine(), out choice);
+
+                switch (choice)
+                {
+
+                    case 1:
+                        {
+                            int n;
+                            Console.Write("Enter integer number ----> ");
+
+                            n = int.Parse(Console.ReadLine());
+
+                            Console.Write("Reverse of " + n + " is " + Operations.ReverseNumber(n));
+                            break;
+                        }
+                    case 2:
+                        {
+                            int n; 
+                            Console.Write("\nEnter integer number ----> ");
+
+                            n = int.Parse(Console.ReadLine());
+                            Console.Write("Reverse of " + n + " is " + Operations.BigestDigit(n));
+
+                            break;
+                        }
+                    case 3:
+                        {
+                            string stringVal = "string 456";
+
+                            Console.Write("Reverse of " + stringVal + " is " + Operations.FirstNum(stringVal));
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Number of element you need enter ");
+                            int numberE = int.Parse(Console.ReadLine());
+
+                            Console.Write( Operations.MaximumInput(numberE));
+
+                            break; 
+
+                        }
+
+
+                    default:
+                        {
+                            if (choice == 5)
+                            {
+                                Console.WriteLine("Thanks for using");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Choose a numer from 1 to 4 and 5 for end ");
+                            }
+                            break;
+                        }
+                }
+            } while (choice != 5);
+
+
+
+
+
+
+        }
+    }
+}
